b338192003d8e4ff8a646a70e0507721f284c939
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-02-26 16:35:05 -0800

    5. Install Nginx web server (w/ Puppet)

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,26 @@
+#configures a new Ubuntu machine with Nginx web server
+package { 'nginx':
+  ensure => 'present'
+}
+
+exec { 'update':
+  command  => 'sudo apt-get update',
+  provider => shell
+}
+
+exec { 'install':
+  command  => 'sudo apt-get -y install nginx',
+  provider => shell
+}
+
+exec { 'echo "Hello World!" | sudo tee /var/www/html/index.html':
+  provider => shell
+}
+
+exec { 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/blog.ehoneahobed.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell
+}
+
+exec { 'sudo service nginx restart':
+  provider => shell
+}
