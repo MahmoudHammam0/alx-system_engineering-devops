231024c21350f9d9d76549aa7ecbabb07ed91c27
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-02-26 16:36:36 -0800

    5. Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -17,7 +17,7 @@ exec { 'echo "Hello World!" | sudo tee /var/www/html/index.html':
   provider => shell
 }
 
-exec { 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/blog.ehoneahobed.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+exec { 'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/google.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
   provider => shell
 }
 
