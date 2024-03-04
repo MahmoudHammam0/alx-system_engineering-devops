074b29ac048364d1f021f3b376f74dee48dbefbd
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-04 05:12:03 -0800

    2. Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,7 +6,7 @@ package { 'nginx':
 file_line { 'add_header':
   match => 'location / {',
   path  => '/etc/nginx/sites-enabled/default',
-  line  => 'location / {\n\t\tadd_header X-Served-By $hostname'
+  line  => 'location / {\n\t\tadd_header X-Served-By $hostname;'
 }
 
 exec { 'restart':
