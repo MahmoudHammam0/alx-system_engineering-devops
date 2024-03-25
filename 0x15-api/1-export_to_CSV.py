7d7cb0c868b496d7a36ba7bb9eeec492294f79da
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-25 06:35:17 -0700

    1. Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     tasks_url = "{}/todos?userId={}".format(main_url, emp_id)
     name_req = requests.get(name_url)
     tasks_req = requests.get(tasks_url)
-    emp_name = name_req.json()[0].get("name")
+    emp_name = name_req.json()[0].get("username")
     with open('{}.csv'.format(emp_id), 'w') as f:
         write = csv.writer(f, quoting=csv.QUOTE_ALL)
         for task in tasks_req.json():
