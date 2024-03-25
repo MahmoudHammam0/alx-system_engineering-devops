5af7071ab299beeef471ba75f745e1e4387eb95b
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-25 06:03:57 -0700

    0. Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'Gather data from an API module'
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    emp_id = sys.argv[1]
+    main_url = "https://jsonplaceholder.typicode.com"
+    name_url = "{}/users?id={}".format(main_url, emp_id)
+    tasks_url = "{}/todos?userId={}".format(main_url, emp_id)
+    name_req = requests.get(name_url)
+    tasks_req = requests.get(tasks_url)
+    emp_name = name_req.json()[0].get("name")
+    no_tasks = len(tasks_req.json())
+    no_done = 0
+    for task in tasks_req.json():
+        if task['completed'] is True:
+            no_done += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(emp_name, no_done, no_tasks))
+    for task in tasks_req.json():
+        if task['completed'] is True:
+            print(" \t{}".format(task['title']))
