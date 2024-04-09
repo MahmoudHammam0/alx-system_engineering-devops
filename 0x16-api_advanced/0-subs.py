f809d0b7152246560f88bd349a8e8456818ec327
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-04-09 11:45:47 -0700

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,17 +8,36 @@ def number_of_subscribers(subreddit):
     function that queries the Reddit API and returns the number of subscribers
     (not active users, total subscribers) for a given subreddit.
     """
+    CLIENT_ID = 'zL8evAsF03DzVk_TArVH3g'
+    SECRET_KEY = 'FT7BTQs-WLEtZEOlH_2mE2QEQ8CV2g'
 
-    if subreddit is None or not isinstance(subreddit, str):
+    auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
+
+    data = {
+        'grant_type': 'password',
+        'username': 'Responsible-Big4336',
+        'password': 'password123'
+    }
+
+    headers = {'User-Agent': 'MyAPI1'}
+
+    res = requests.post('https://www.reddit.com/api/v1/access_token',
+                        auth=auth, data=data, headers=headers)
+
+    TOKEN = res.json()['access_token']
+
+    headers['Authorization'] = "bearer {}".format(TOKEN)
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, auth=auth, data=data, headers=headers,
+                            allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+    d = response.json()
+    if 'data' not in d:
+        return 0
+    if 'subscribers' not in d.get('data'):
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = requests.get(url, headers=user_agent)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
-        return 0
+    return d['data']['subscribers']
