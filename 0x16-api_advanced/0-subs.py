8b76e8d41f4f7f4e0aa7ab2d592cda9f6f2656d7
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-04-09 16:14:33 -0700

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,8 @@ def number_of_subscribers(subreddit):
         'password': '0170398745Mh'
     }
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    res = requests.post('https://www.reddit.com/api/v1/access_token', auth=auth, data=data, headers=headers)
+    res = requests.post('https://www.reddit.com/api/v1/access_token',
+                        auth=auth, data=data, headers=headers)
     token = res.json()['access_token']
     headers['Authorization'] = 'bearer {}'.format(token)
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
