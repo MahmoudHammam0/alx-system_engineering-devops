6eb19f7d94ca512b9caf3513a56df8997797736b
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-04-09 17:23:36 -0700

    2. Recurse it!

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'Top Ten module'
+import requests
+
+
+def recurse(subreddit, hot_list=[], i=0):
+    'returns a list of titles of all hot articles for a given subreddit'
+    clientId = 'J2BkSoEGIIGkNRe4aVwAnw'
+    secretKey = 'sKObr2oIPuhq88fqo2U1_TDeOu42eA'
+    auth = requests.auth.HTTPBasicAuth(clientId, secretKey)
+    data = {
+            'grant_type': 'password',
+            'username': 'Mahmoudhammam',
+            'password': '0170398745Mh'
+    }
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    res = requests.post('https://www.reddit.com/api/v1/access_token',
+            auth=auth, data=data, headers=headers)
+    token = res.json()['access_token']
+    headers['Authorization'] = 'bearer {}'.format(token)
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    req = requests.get(url, auth=auth, data=data, headers=headers,
+            allow_redirects=False)
+    res = req.json()['data']['children']
+    if len(hot_list) != len(res):
+        print(res[i]['data']['title'])
+        hot_list.append(res[i]['data']['title'])
+        i += 1
+        recurse(subreddit, hot_list, i)
+    else:
+        return hot_list
