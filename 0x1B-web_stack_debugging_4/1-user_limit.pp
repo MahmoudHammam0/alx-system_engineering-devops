b9fa2adc5c60b1763253b5e5037f5b32d7d62c70
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-04-18 05:29:19 -0700

    0x1B. Web stack debugging #4

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,9 @@
+#User limits
+exec { 'increase_users_hard_limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+exec { 'increase_users_soft_limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
